d7f7b9984bf3f74b9cdaefc981e2e1b37b442c95
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-04-02 22:38:02 +0200

    exercice 1 to 2

---
+++ partie1.py
@@ -0,0 +1,29 @@
+"""Module partie1
+https://www.w3resource.com/python-exercises/python-basic-exercises.php
+"""
+
+import sys
+
+
+def exercice1():
+    """
+    Write a Python program to print the following string in a specific format (see the output).
+    Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a
+    diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" Output :
+    """
+    print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a "
+          "diamond in the sky.\nTwinkle, twinkle, little star, \n\tHow I wonder what you are")
+
+
+def exercice2():
+    """
+    Write a Python program to get the Python version you are using.
+    """
+    print(sys.version)
+
+
+if __name__ == '__main__':
+    print("exercicer1: ")
+    exercice1()
+    print("\nexercice2: ")
+    exercice2()
